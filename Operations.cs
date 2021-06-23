adb5b8579af2d2d7f0402c9fc74a78e9ac7d76ae
Diwakar <diwakarn1599@gmail.com>
2021-06-23 11:38:01 +0530

    [Diwakar]First Commit

---
+++ Operations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Operations
+    {
+        public int multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int var1 = 1;
+             string var2 = "MyVariable: ";
+             int var3 = 2;
+
+             Console.WriteLine(var2+var1);
+             Console.WriteLine(add(var1, var3));*/
+            int num1, num2;
+            Console.WriteLine("Enter First Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            num2 = Convert.ToInt32( Console.ReadLine());
+
+            Operations op = new Operations();
+            
+            Console.WriteLine("Multiplication :"+op.multiply(num1, num2));
+        }
+
+        private static int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
